d0d6549716efcbadfc5ea1432bb692f2ba14d29f
Kaleb Ribeiro <106794366+KalebRiber@users.noreply.github.com>
2022-08-05 16:58:46 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# itoa.c
+
+My Integer to String conversion implementation in C just by using two standard headers.
---
+++ itoa.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef unsigned char byte;
+
+char * itoa(int number) {
+  int i = 1,
+    n = 0,
+    mul = i;
+
+  byte p = number < n;
+
+  if (p) {
+    number *= -1;
+    i++;
+  }
+
+  while (number % mul != number && i++) mul *= 10;
+  mul = 10;
+
+  byte * cnumber = malloc(sizeof(byte) * i--);
+
+  for (int x = 0; x <= i; x++)
+    *
+    (cnumber + x) = 0x00;
+
+  while (i--> p) {
+    n = (number % mul);
+    *(cnumber + i) = n / (mul / 10) + 0x30;
+    number -= n;
+    mul *= 10;
+  }
+
+  if (! * cnumber)
+    *
+    cnumber = '-';
+
+  return cnumber;
+}
+
+int main(int argc, char * argv[]) {
+  char * number = itoa(12345),
+    * negative_number = itoa(-12345);
+
+  printf("Number (positive):  %s\r\n"
+    "Number (negative): %s\r\n", number, negative_number);
+
+  free(number);
+  free(negative_number);
+
+  return 0;
+}
